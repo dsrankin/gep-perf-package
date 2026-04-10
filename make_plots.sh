90533b8a5889ab7414566292eadc7aadeb509ade
Dylan Rankin <drankin@bu.edu>
2026-04-10 15:31:01 +0200

    config updates

--- make_plots.sh
+++ make_plots.sh
@@ -52,8 +52,10 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "jet" ]]; then
     plot_cmd perf_results/Jet_ttbar_{A*422Jets,A*$CELLSEL*TowerJets,L*jFexSRJetRoI}_n2_rate50.npz --name jet_ttbar --plotlabel _rate50$CELLLAB --plottext "50 kHz [TTbar]" --nobj 2 --xmax 150. --noerr
     plot_cmd perf_results/Jet_ttbar_{A*422Jets,A*$CELLSEL*TowerJets,L*jFexSRJetRoI}_n3_rate75.npz --name jet_ttbar --plotlabel _rate75$CELLLAB --plottext "75 kHz [TTbar]" --nobj 3 --xmax 150. --noerr
     plot_cmd perf_results/Jet_ttbar_{A*422Jets,A*$CELLSEL*TowerJets,L*jFexSRJetRoI}_n4_rate100.npz --name jet_ttbar --plotlabel _rate100$CELLLAB --plottext "100 kHz [TTbar]" --nobj 4 --xmax 100. --noerr
+fi
     
 ## SKJets
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "skjet" ]]; then
     echo "SKJets"
     PLOT_SUBDIR="skjet"
     plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}SKJets_n1_pt100.npz --name skjet --plotlabel _pt100$CELLLAB --plottext "J100 [VBF HH->4b]" --nobj 1 --xmax 200. --noerr --dofulleff --dorespresol
@@ -86,8 +88,46 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "jet" ]]; then
     plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}SKJets_n2_rate50.npz --name skjet_ttbar --plotlabel _rate50$CELLLAB --plottext "50 kHz [TTbar]" --nobj 2 --xmax 150. --noerr
     plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}SKJets_n3_rate75.npz --name skjet_ttbar --plotlabel _rate75$CELLLAB --plottext "75 kHz [TTbar]" --nobj 3 --xmax 150. --noerr
     plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}SKJets_n4_rate100.npz --name skjet_ttbar --plotlabel _rate100$CELLLAB --plottext "100 kHz [TTbar]" --nobj 4 --xmax 100. --noerr
+fi
+    
+## EtaSKJets
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "etaskjet" ]]; then
+    echo "EtaSKJets"
+    PLOT_SUBDIR="etaskjet"
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_pt100.npz --name etaskjet --plotlabel _pt100$CELLLAB --plottext "J100 [VBF HH->4b]" --nobj 1 --xmax 200. --noerr --dofulleff --dorespresol
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_pt60.npz --name etaskjet --plotlabel _pt60$CELLLAB --plottext "2J60 [VBF HH->4b]" --nobj 2 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_pt50.npz --name etaskjet --plotlabel _pt50$CELLLAB --plottext "3J50 [VBF HH->4b]" --nobj 3 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_pt40.npz --name etaskjet --plotlabel _pt40$CELLLAB --plottext "4J40 [VBF HH->4b]" --nobj 4 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_rate50.npz --name etaskjet --plotlabel _rate50$CELLLAB --plottext "50 kHz [VBF HH->4b]" --nobj 1 --xmax 200. --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_rate50.npz --name etaskjet --plotlabel _rate50$CELLLAB --plottext "50 kHz [VBF HH->4b]" --nobj 2 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_rate75.npz --name etaskjet --plotlabel _rate75$CELLLAB --plottext "75 kHz [VBF HH->4b]" --nobj 3 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_rate100.npz --name etaskjet --plotlabel _rate100$CELLLAB --plottext "100 kHz [VBF HH->4b]" --nobj 4 --xmax 100. --noerr
+    
+    echo "Boosted EtaSKJets"
+    PLOT_SUBDIR="boosted_etaskjet"
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_boosted_pt100.npz --name boosted_etaskjet --plotlabel _boosted_pt100$CELLLAB --plottext "J100 (Boosted) [VBF HH->4b]" --nobj 1 --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_boosted_pt60.npz --name boosted_etaskjet --plotlabel _boosted_pt60$CELLLAB --plottext "2J60 (Boosted) [VBF HH->4b]" --nobj 2 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_boosted_pt50.npz --name boosted_etaskjet --plotlabel _boosted_pt50$CELLLAB --plottext "3J50 (Boosted) [VBF HH->4b]" --nobj 3 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_boosted_pt40.npz --name boosted_etaskjet --plotlabel _boosted_pt40$CELLLAB --plottext "4J40 (Boosted) [VBF HH->4b]" --nobj 4 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_boosted_rate50.npz --name boosted_etaskjet --plotlabel _boosted_rate50$CELLLAB --plottext "50 kHz (Boosted) [VBF HH->4b]" --nobj 1 --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_boosted_rate50.npz --name boosted_etaskjet --plotlabel _boosted_rate50$CELLLAB --plottext "50 kHz (Boosted) [VBF HH->4b]" --nobj 2 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_boosted_rate75.npz --name boosted_etaskjet --plotlabel _boosted_rate75$CELLLAB --plottext "75 kHz (Boosted) [VBF HH->4b]" --nobj 3 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_boosted_rate100.npz --name boosted_etaskjet --plotlabel _boosted_rate100$CELLLAB --plottext "100 kHz (Boosted) [VBF HH->4b]" --nobj 4 --xmax 100. --noerr
+
+    echo "EtaSKJets"
+    PLOT_SUBDIR="etaskjet_ttbar"
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_pt100.npz --name etaskjet_ttbar --plotlabel _pt100$CELLLAB --plottext "J100 [TTbar]" --nobj 1 --xmax 200. --noerr --dofulleff --dorespresol
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_pt60.npz --name etaskjet_ttbar --plotlabel _pt60$CELLLAB --plottext "2J60 [TTbar]" --nobj 2 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_pt50.npz --name etaskjet_ttbar --plotlabel _pt50$CELLLAB --plottext "3J50 [TTbar]" --nobj 3 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_pt40.npz --name etaskjet_ttbar --plotlabel _pt40$CELLLAB --plottext "4J40 [TTbar]" --nobj 4 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_rate50.npz --name etaskjet_ttbar --plotlabel _rate50$CELLLAB --plottext "50 kHz [TTbar]" --nobj 1 --xmax 200. --noerr
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_rate50.npz --name etaskjet_ttbar --plotlabel _rate50$CELLLAB --plottext "50 kHz [TTbar]" --nobj 2 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_rate75.npz --name etaskjet_ttbar --plotlabel _rate75$CELLLAB --plottext "75 kHz [TTbar]" --nobj 3 --xmax 150. --noerr
+    plot_cmd perf_results/Jet_ttbar_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_rate100.npz --name etaskjet_ttbar --plotlabel _rate100$CELLLAB --plottext "100 kHz [TTbar]" --nobj 4 --xmax 100. --noerr
+fi
     
 ## Jets (VBF)
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "jet" ]]; then
     echo "VBF Jets"
     PLOT_SUBDIR="vbf_jet"
     plot_cmd perf_results/Jet_dijet_mass_*{422Jets,$CELLSEL*TowerJets,jFexSRJetRoI}_n1_pt100.npz --name jet --plotlabel _mjj_pt100$CELLLAB --plottext "J100 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
@@ -109,9 +149,10 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "jet" ]]; then
     plot_cmd perf_results/Jet_dijet_mass_*{422Jets,$CELLSEL*TowerJets,jFexSRJetRoI}_n2_boosted_rate50.npz --name boosted_jet --plotlabel _mjj_boosted_rate50$CELLLAB --plottext "50 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
     plot_cmd perf_results/Jet_dijet_mass_*{422Jets,$CELLSEL*TowerJets,jFexSRJetRoI}_n3_boosted_rate75.npz --name boosted_jet --plotlabel _mjj_boosted_rate75$CELLLAB --plottext "75 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
     plot_cmd perf_results/Jet_dijet_mass_*{422Jets,$CELLSEL*TowerJets,jFexSRJetRoI}_n4_boosted_rate100.npz --name boosted_jet --plotlabel _mjj_boosted_rate100$CELLLAB --plottext "100 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+fi
 
 ## SKJets (VBF)
-
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "skjet" ]]; then
     echo "VBF SKJets"
     PLOT_SUBDIR="vbf_skjet"
     plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}SKJets_n1_pt100.npz --name skjet --plotlabel _mjj_pt100$CELLLAB --plottext "J100 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
@@ -135,6 +176,32 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "jet" ]]; then
     plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}SKJets_n4_boosted_rate100.npz --name boosted_skjet --plotlabel _mjj_boosted_rate100$CELLLAB --plottext "100 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
 fi
 
+## EtaSKJets (VBF)
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "etaskjet" ]]; then
+    echo "VBF EtaSKJets"
+    PLOT_SUBDIR="vbf_etaskjet"
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_pt100.npz --name etaskjet --plotlabel _mjj_pt100$CELLLAB --plottext "J100 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_pt60.npz --name etaskjet --plotlabel _mjj_pt60$CELLLAB --plottext "2J60 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_pt50.npz --name etaskjet --plotlabel _mjj_pt50$CELLLAB --plottext "3J50 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_pt40.npz --name etaskjet --plotlabel _mjj_pt40$CELLLAB --plottext "4J40 [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_rate50.npz --name etaskjet --plotlabel _mjj_rate50$CELLLAB --plottext "50 kHz [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_rate50.npz --name etaskjet --plotlabel _mjj_rate50$CELLLAB --plottext "50 kHz [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_rate75.npz --name etaskjet --plotlabel _mjj_rate75$CELLLAB --plottext "75 kHz [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_rate100.npz --name etaskjet --plotlabel _mjj_rate100$CELLLAB --plottext "100 kHz [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    
+    echo "VBF Boosted EtaSKJets"
+    PLOT_SUBDIR="vbf_boosted_etaskjet"
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_boosted_pt100.npz --name boosted_etaskjet --plotlabel _mjj_boosted_pt100$CELLLAB --plottext "J100 (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_boosted_pt60.npz --name boosted_etaskjet --plotlabel _mjj_boosted_pt60$CELLLAB --plottext "2J60 (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_boosted_pt50.npz --name boosted_etaskjet --plotlabel _mjj_boosted_pt50$CELLLAB --plottext "3J50 (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_boosted_pt40.npz --name boosted_etaskjet --plotlabel _mjj_boosted_pt40$CELLLAB --plottext "4J40 (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_boosted_rate50.npz --name boosted_etaskjet --plotlabel _mjj_boosted_rate50$CELLLAB --plottext "50 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_boosted_rate50.npz --name boosted_etaskjet --plotlabel _mjj_boosted_rate50$CELLLAB --plottext "50 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n3_boosted_rate75.npz --name boosted_etaskjet --plotlabel _mjj_boosted_rate75$CELLLAB --plottext "75 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+    plot_cmd perf_results/Jet_dijet_mass_A{*422,*$CELLSEL*Tower}EtaSKJets_n4_boosted_rate100.npz --name boosted_etaskjet --plotlabel _mjj_boosted_rate100$CELLLAB --plottext "100 kHz (Boosted) [VBF HH->4b]" --nobj 0 --noerr --xmax -1
+fi
+
+
 ## Taus
 if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "tau" ]]; then
     echo "Taus"
@@ -143,15 +210,26 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "tau" ]]; then
     plot_cmd perf_results/Tau_*{422Jets,$CELLSEL*TowerJets,eTauRoISim,jFexSRJetRoI}_n2_pt40.npz --name tau --plotlabel _pt40$CELLLAB --plottext "2Tau40 [y*->tt]" --nobj 2 --xmax 100. --noerr --dofulleff
     plot_cmd perf_results/Tau_*{422Jets,$CELLSEL*TowerJets,eTauRoISim,jFexSRJetRoI}_n1_rate10.npz --name tau --plotlabel _rate10$CELLLAB --plottext "10 kHz [y*->tt]" --nobj 1 --xmax 230. --noerr
     plot_cmd perf_results/Tau_*{422Jets,$CELLSEL*TowerJets,eTauRoISim,jFexSRJetRoI}_n2_rate200.npz --name tau --plotlabel _rate200$CELLLAB --plottext "200 kHz [y*->tt]" --nobj 2 --xmax 100. --noerr
+fi
 
 ## SKTaus
-
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "sktau" ]]; then
     echo "SKTaus"
     PLOT_SUBDIR="sktau"
     plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}SKJets_n1_pt150.npz --name sktau --plotlabel _pt150$CELLLAB --plottext "Tau150 [y*->tt]" --nobj 1 --xmax 250. --noerr --dofulleff --dorespresol
     plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}SKJets_n2_pt40.npz --name sktau --plotlabel _pt40$CELLLAB --plottext "2Tau40 [y*->tt]" --nobj 2 --xmax 100. --noerr --dofulleff
     plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}SKJets_n1_rate10.npz --name sktau --plotlabel _rate10$CELLLAB --plottext "10 kHz [y*->tt]" --nobj 1 --xmax 250. --noerr
     plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}SKJets_n2_rate200.npz --name sktau --plotlabel _rate200$CELLLAB --plottext "200 kHz [y*->tt]" --nobj 2 --xmax 100. --noerr
+fi
+
+## EtaEtaSKTaus
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "etasktau" ]]; then
+    echo "EtaSKTaus"
+    PLOT_SUBDIR="etasktau"
+    plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_pt150.npz --name etasktau --plotlabel _pt150$CELLLAB --plottext "Tau150 [y*->tt]" --nobj 1 --xmax 250. --noerr --dofulleff --dorespresol
+    plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_pt40.npz --name etasktau --plotlabel _pt40$CELLLAB --plottext "2Tau40 [y*->tt]" --nobj 2 --xmax 100. --noerr --dofulleff
+    plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}EtaSKJets_n1_rate10.npz --name etasktau --plotlabel _rate10$CELLLAB --plottext "10 kHz [y*->tt]" --nobj 1 --xmax 250. --noerr
+    plot_cmd perf_results/Tau_A{*422,*$CELLSEL*Tower}EtaSKJets_n2_rate200.npz --name etasktau --plotlabel _rate200$CELLLAB --plottext "200 kHz [y*->tt]" --nobj 2 --xmax 100. --noerr
 fi
 
 ## Eles
@@ -223,13 +301,24 @@ if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "met" ]]; then
     plot_cmd perf_results/MET_{*422,*Offline*Tower,*$CELLSEL*Tower}_n1_pt150.npz --name met --plotlabel _pt150$CELLLAB --plottext "MET150 [ZH->vvbb]" --nobj 1 --xmax 1000. --noerr --dofulleff --dorespresol
     plot_cmd perf_results/MET_{*422,*Offline*Tower,*$CELLSEL*Tower}_n1_pt200.npz --name met --plotlabel _pt200$CELLLAB --plottext "MET200 [ZH->vvbb]" --nobj 1 --xmax 1000. --noerr --dofulleff
     plot_cmd perf_results/MET_{*422,*Offline*Tower,*$CELLSEL*Tower}_n1_rate60.npz --name met --plotlabel _rate60$CELLLAB --plottext "60 kHz [ZH->vvbb]" --nobj 1 --xmax 1000. --noerr
+fi
     
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "skmet" ]]; then
     echo "SKMET"
     PLOT_SUBDIR="skmet"
     plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerSK_n1_pt150.npz --name skmet --plotlabel _pt150$CELLLAB --plottext "MET150 [ZH->vvbb]" --nobj 1 --xmax 500. --noerr --dofulleff --dorespresol
     plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerSK_n1_pt200.npz --name skmet --plotlabel _pt200$CELLLAB --plottext "MET200 [ZH->vvbb]" --nobj 1 --xmax 500. --noerr --dofulleff
     plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerSK_n1_rate60.npz --name skmet --plotlabel _rate60$CELLLAB --plottext "60 kHz [ZH->vvbb]" --nobj 1 --xmax 500. --noerr
 fi
+
+if [[ "$OBJTYPE" == "all" ]] || [[ "$OBJTYPE" == "etaskmet" ]]; then
+    echo "EtaSKMET"
+    PLOT_SUBDIR="etaskmet"
+    plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerEtaSK_n1_pt150.npz --name etaskmet --plotlabel _pt150$CELLLAB --plottext "MET150 [ZH->vvbb]" --nobj 1 --xmax 500. --noerr --dofulleff --dorespresol
+    plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerEtaSK_n1_pt200.npz --name etaskmet --plotlabel _pt200$CELLLAB --plottext "MET200 [ZH->vvbb]" --nobj 1 --xmax 500. --noerr --dofulleff
+    plot_cmd perf_results/MET_*{Offline,$CELLSEL}*TowerEtaSK_n1_rate60.npz --name etaskmet --plotlabel _rate60$CELLLAB --plottext "60 kHz [ZH->vvbb]" --nobj 1 --xmax 500. --noerr
+fi
+
 
 if [[ "$MAKETAR" == "true" ]]; then
     tar -cvz -f perf_plots.tar.gz "${PLOTDIR_BASE}/"
