717417ca64ce0518d953ac7333fdbd3dd05de503
Dylan Rankin <drankin@bu.edu>
2026-04-20 19:00:54 +0200

    script updates

--- run.sh
+++ run.sh
@@ -1,18 +1,41 @@
-gep-perf run configs/jet_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/jet_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/jet_example_sig4.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/jet_tthad_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/jet_tthad_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/jet_tthad_example_sig4.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/tau_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/tau_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/tau_example_sig4.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/pho_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/pho_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/pho_example_sig4.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/ele_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/ele_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/ele_example_sig4.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/met_example.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/met_example_sig3.yaml --plotdir perf_plots --resdir perf_results
-gep-perf run configs/met_example_sig4.yaml --plotdir perf_plots --resdir perf_results
+#!/bin/bash
+
+OBJECT="$1"
+shift
+
+for TYPE in "$@"; do
+  echo "$OBJECT $TYPE"
+
+  [ "$OBJECT" = "jet" ] || [ "$OBJECT" = "all" ] && {
+    gep-perf run configs/jet_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/jet_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/jet_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/jet_tthad_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/jet_tthad_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/jet_tthad_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+  }
+
+  [ "$OBJECT" = "tau" ] || [ "$OBJECT" = "all" ] && {
+    gep-perf run configs/tau_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/tau_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/tau_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+  }
+
+  [ "$OBJECT" = "pho" ] || [ "$OBJECT" = "all" ] && {
+    gep-perf run configs/pho_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/pho_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/pho_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+  }
+
+  [ "$OBJECT" = "ele" ] || [ "$OBJECT" = "all" ] && {
+    gep-perf run configs/ele_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/ele_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/ele_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+  }
+
+  [ "$OBJECT" = "met" ] || [ "$OBJECT" = "all" ] && {
+    gep-perf run configs/met_example.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/met_example_sig3.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+    gep-perf run configs/met_example_sig4.yaml --plotdir perf_plots --resdir perf_results --collection-sets "$TYPE"
+  }
+done
--- runplots.sh
+++ runplots.sh
@@ -2,9 +2,9 @@
 
 for TYPE in "$@"; do
   echo $TYPE
-  ./make_plots.sh $TYPE E6LSB50 _n6lsb50 false
-  ./make_plots.sh $TYPE LSB50G2 _lsb50g2 false
-  ./make_plots.sh $TYPE SIG2 _2sig false
+  #./make_plots.sh $TYPE E6LSB50 _n6lsb50 false
+  #./make_plots.sh $TYPE LSB50G2 _lsb50g2 false
+  #./make_plots.sh $TYPE SIG2 _2sig false
   ./make_plots.sh $TYPE SIG3 _3sig false
   ./make_plots.sh $TYPE SIG4 _4sig true
 done
